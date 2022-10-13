7af7c214927f7562a4fd399a655a9b39060f66f1
Nalx602 <alexandru602@gmail.com>
2022-10-13 17:38:16 +0100

    added some default routers and nodemon devdepency

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-//As a convention, in this file we should only configure stuff only in regards to our Express app, which will
+//As a convention, in this file we should only configure stuff only in regards to our Express app,
 
 const express = require('express');
+const projectRouter = require('./routes/projectRoutes');
+const teamRouter = require('./routes/teamRoutes');
+const userRouter = require('./routes/userRoutes');
+const ticketRouter = require('./routes/ticketRoutes');
 
-const app = express(); // it creates an Express JS application
+const app = express(); // creating an Express.JS application
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Express app listening on port ${port}.`);
-});
+//        Mounting the subrouters to the app
+
+app.use('/api/v1/projects', projectRouter);
+app.use('/api/v1/teams', teamRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tickets', ticketRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const dotenv = require('dotenv');
+const app = require('./app');
 
-//dotenv is a module that loads environment variables from config.env file into process.env
+//dotenv is a module that loads environment variables from config.env file into process.env;
 dotenv.config({ path: './config.env' });
-console.log('hello world');
+
+//Starting the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`);
+});
